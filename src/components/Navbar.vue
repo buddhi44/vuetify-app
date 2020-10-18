08d03cc777579e8b7d9c0b49bc39e22e239a3201
bc <bmpiyasena4@gmail.com>
2020-10-18 15:49:02 +0530

    2 nd edit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+  <v-card flat>
+    <v-toolbar
+      color="primary"
+      dark
+      flat
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </v-toolbar>
+
+  
+      
+
+      <v-divider></v-divider>
+
+      
+    
+  </v-card>
+
+  
+  </div>
+</template>
---
+++ src/components/home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div >
+    <h1>This is an Home page</h1>
+     <div>
+         <slider></slider>
+          <card></card>
+    </div>          
+ </div>
+</template>
+
+<script>
+import Slider from './Home/slider.vue'
+import Card from './Home/card.vue'
+  export default {
+
+   components: { 'slider' : Slider,
+                  'card'  : Card,
+            },
+  
+    data: () => ({
+      model: 'caption',
+      sizes: [
+        ['mdi-devices', 'all', 'caption'],
+        ['mdi-cellphone-iphone', 'sm', 'body-2'],
+        ['mdi-laptop', 'md', 'body-1'],
+        ['mdi-monitor', 'lg', 'h6'],
+        ['mdi-television', 'xl', 'h4'],
+      ],
+    }),
+
+    computed: {
+      typeClass () {
+        const type = ['text', this.model]
+        const [, breakpoint] = this.current
+
+        if (breakpoint !== 'all') {
+          type.splice(1, 0, breakpoint)
+        }
+
+        return `.${type.join('-')}`
+      },
+      current () {
+        return this.sizes.find(size => size[2] === this.model)
+      },
+    },
+  }
+</script>
